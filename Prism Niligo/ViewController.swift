7c96ca8d1011dc7544b67ea72bdd37e22689a517
amirhosein-abbasi <amirhosein.abbasi594@gmail.com>
2017-07-18 14:10:42 +0430

    Initial Commit

---
+++ Prism Niligo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prism Niligo
+//
+//  Created by amirhosein on 4/27/1396 AP.
+//  Copyright © 1396 amirhosein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
